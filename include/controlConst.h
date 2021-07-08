a898862b0e52cbb772d6d971bc1d46f1d7b93d51
wa3401 <69916271+wa3401@users.noreply.github.com>
2021-07-08 09:46:21 -0500

    Added comments to two color detection and controlConst

--- include/controlConst.h
+++ include/controlConst.h
@@ -1,3 +1,12 @@
+/**
+ * 
+ * @author Will Anderson
+ * 
+ * This file handels the steering calculation based on which lane lines are found and where in the image they are
+ * I have used a hyperbolic tangent function to control the steering of the vehicle since this allows for maxing out the angle at 0.4, the max steering angle of the car
+ * 
+ **/
+
 #include <ros/ros.h>
 #include <image_transport/image_transport.h>
 #include <cv_bridge/cv_bridge.h>
@@ -17,9 +26,12 @@ public:
     static vector<double> steerSpeed(vector<vector<double>> yellowLaneLines, vector<vector<double>> whiteLaneLines, int laneNumber)
     {
         double steeringAngle;
+
+        /** -------------------------------------------------**\
+        * ------------------Define Constants------------------ *
+        \**--------------------------------------------------**/
         double carSpeed;
         double laneMidPointCorrectionCoefficient = (M_PI / 6) / 640 * 1.25;
-
         double TURN_CONST_HARD_OUT = 3.0;
         double TURN_CONST_SOFT_OUT = 4.0;
         double TURN_CONST_HARD_IN = 2.5;
@@ -27,14 +39,17 @@ public:
         double CENT_CORR_CONST = -0.0005;
         double SPEED_CONST = 0.75;
 
-        //Right Lane
+        /** -------------------------------------------------**\
+        * -------------YELLOW & WHITE LANES FOUND------------- *
+        \**--------------------------------------------------**/
         if (static_cast<int>(yellowLaneLines.size()) >= 1 && static_cast<int>(whiteLaneLines.size()) >= 1)
         {
             ROS_INFO("TWO LANES FOUND");
-            double wSlope = whiteLaneLines.at(0).at(1);
-            double wXCoord = whiteLaneLines.at(0).at(0);
-            double ySlope = yellowLaneLines.at(0).at(1);
-            double yXCoord = yellowLaneLines.at(0).at(0);
+            //Get solpes and intercepts from vectors
+            double wSlope = whiteLaneLines.at(0).at(1); //Slope of the white line
+            double wXCoord = whiteLaneLines.at(0).at(0); //X coordinate of the white line (at the bottom of the image)
+            double ySlope = yellowLaneLines.at(0).at(1); //Slope of the yellow line
+            double yXCoord = yellowLaneLines.at(0).at(0); //X coordinate of the yellow line (at the bottom of the image)
             double distFromCenter = 0;
             if (laneNumber == 0)
             {
@@ -45,19 +60,26 @@ public:
                 distFromCenter = 462 - (wXCoord + yXCoord) / 2;
             }
             ROS_INFO("Center Steering Correction : %s", std::to_string(distFromCenter * CENT_CORR_CONST).c_str());
+
+            //Need these two calculations as the camera is not centered on the car
+            //This allows for average slope to account for the non-centered image and calculate steering angle from there
             double difInSlope = abs(wSlope) - abs(ySlope);
             double avgSlope = (wSlope + ySlope) / 2 + difInSlope / 2;
+            //Using hyperbolic tangent * the max steering angle to find the necesary steering angle
             steeringAngle = 0.4 * tanh(avgSlope / TURN_CONST_HARD_OUT) + CENT_CORR_CONST * distFromCenter;
             carSpeed = SPEED_CONST;
         }
-        //Inside lanes
+        /** -------------------------------------------------**\
+        * ---------------CONTROL FOR INNER LANES-------------- *
+        \**--------------------------------------------------**/
         else if (static_cast<int>(whiteLaneLines.size()) == 0 && static_cast<int>(yellowLaneLines.size()) >= 1)
         {
             ROS_INFO("ONLY YELLOW LANE FOUND");
-            ///TODO: No white lane found but yellow lane found
+            ///No white lane found but yellow lane found
             //Most likely making a right turn in right la
-            double ySlope = yellowLaneLines.at(0).at(1) / 1.5;
+            double ySlope = yellowLaneLines.at(0).at(1) / 1.5; //This /1.5 just works since we do not have 2 slopes to average out
             double yXCoord = yellowLaneLines.at(0).at(0);
+
             if (laneNumber == 0 && yXCoord <= 500)
             {
                 ROS_INFO("LEFT LANE HARD LEFT TURN");
@@ -88,6 +110,7 @@ public:
                 carSpeed = 0.75;
             }
 
+            //Center correction control once the car is almopst fully around the corner (when the slope returns to less than 1.5)
             if (abs(ySlope) < 1.5 && laneNumber == 0)
             {
                 steeringAngle += CENT_CORR_CONST * (820 - yXCoord);
@@ -98,13 +121,15 @@ public:
             }
             carSpeed = SPEED_CONST;
         }
-        //Outside Lanes
+        /** -------------------------------------------------**\
+        * ---------------CONTROL FOR OUTER LANES-------------- *
+        \**--------------------------------------------------**/
         else if (static_cast<int>(yellowLaneLines.size()) == 0 && static_cast<int>(whiteLaneLines.size()) >= 1)
         {
             ROS_INFO("ONLY WHITE LANE FOUND");
-            ///TODO: No yelow lane found but white lane found
+            ///No yelow lane found but white lane found
             //Most likely making a left turn in the right lane
-            double wSlope = whiteLaneLines.at(0).at(1) / 1.5;
+            double wSlope = whiteLaneLines.at(0).at(1) / 1.5; //This /1.5 just works since we do not have 2 slopes to average out
             double wXCoord = whiteLaneLines.at(0).at(0);
             if (laneNumber == 0 && wXCoord >= 500)
             {
@@ -136,6 +161,7 @@ public:
                 carSpeed = 0.75;
             }
 
+            //Center correction control once the car is almopst fully around the corner (when the slope returns to less than 1.5)
             if (abs(wSlope) < 1.5 && laneNumber == 1)
             {
                 steeringAngle += CENT_CORR_CONST * (820 - wXCoord);
@@ -146,6 +172,9 @@ public:
             }
             carSpeed = SPEED_CONST;
         }
+        /** -------------------------------------------------**\
+        * -------------------NO LANES FOUND------------------- *
+        \**--------------------------------------------------**/
         else
         {
             ROS_INFO("No lane lines found");
@@ -167,37 +196,5 @@ public:
         speedSteer.push_back(steeringAngle);
 
         return speedSteer;
-    }
-
-    static int
-    laneFinder(vector<vector<double>> wLines, vector<vector<double>> yLines, int lane)
-    {
-        int laneFind = lane;
-        if (laneFind > -2)
-        {
-            if (yLines.size() > 0)
-            {
-                if (yLines.at(0).at(0) > 480)
-                {
-                    laneFind = 0;
-                }
-                else
-                {
-                    laneFind = 1;
-                }
-            }
-            else if (wLines.size() > 0)
-            {
-                if (wLines.at(0).at(0) < 480)
-                {
-                    laneFind = 0;
-                }
-                else
-                {
-                    laneFind = 1;
-                }
-            }
-        }
-        return laneFind;
     }
 };
--- src/twoColorDetection.cpp
+++ src/twoColorDetection.cpp
@@ -1,3 +1,12 @@
+/**
+ * @Author Ryan Brown, Will Anderson
+ * 
+ * This is a modification of the rosImageToCV.cpp file in this folder. 
+ * This file processes the white and yellow lines into different images so that the car can tell which lane it is in, improving the lane following algorithm.
+ * 
+ * ^^^^^USE DEBUGER TO RUN THIS FILE^^^^^
+ **/
+
 #include <ros/ros.h>
 #include <image_transport/image_transport.h>
 #include <cv_bridge/cv_bridge.h>
@@ -42,6 +51,8 @@ public:
         ///camera/color/image_raw
         std::string drive_topic;
         drive_topic = "/vesc/low_level/ackermann_cmd_mux/input/navigation";
+
+        // Subscribes to this topic, which is published by realsense camera node (This is started in the launch file)
         image_sub_ = image_transport.subscribe("/camera/color/image_raw", 1, &SensorTOCV::image_callback, this);
         image_pub_ = image_transport.advertise("/testOutputCam", 1);
         drive_pub = n.advertise<ackermann_msgs::AckermannDriveStamped>(drive_topic, 10);
@@ -49,32 +60,32 @@ public:
         namedWindow(OPENCV_WINDOW_WHITE);
         namedWindow(OTHER_WINDOW_YELLOW);
         namedWindow(OTHER_WINDOW_WHITE);
-
-        //used for testing
-        // namedWindow("Trackbars",(640,200));
-        // createTrackbar("Hue min","Trackbars",&hmin,179);
-        // createTrackbar("Hue max","Trackbars",&hmax,179);
-        // createTrackbar("Sat min","Trackbars",&smin,255);
-        // createTrackbar("Sat max","Trackbars",&smax,255);
-        // createTrackbar("Val min","Trackbars",&vmin,255);
-        // createTrackbar("Val max","Trackbars",&vmax,255);
     }
     ~SensorTOCV()
     {
         cv::destroyAllWindows();
     }
 
+    // Called every time an image is published to this topic
     void image_callback(const sensor_msgs::ImageConstPtr &msg)
     {
+        //Creates an open cv bridge
         cv_bridge::CvImagePtr cv_ptr;
+        //Create opencv mats for the color detection images after canny edge detection
         Mat woutput;
         Mat youtput;
+        //Creates the displays for the hough lines
         Mat displayForYellowLines(270, 960, CV_8UC3, Scalar(0, 0, 0));
         Mat displayForWhiteLines(270, 960, CV_8UC3, Scalar(0, 0, 0));
 
         //CV_8UC1
         try
         {
+
+            /** -------------------------------------------------**\
+            * ------------------Image Processing------------------ *
+            \**--------------------------------------------------**/
+
             // for color camera
             cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
 
@@ -87,12 +98,12 @@ public:
             Mat croppedImage;
             croppedImage = cv_ptr->image(crop);
 
-            //White values
+            //White mins and maxes for Hue-Saturation-Value model
             int whmin = 34, whmax = 179, wsmin = 0, wsmax = 255, wvmin = 252, wvmax = 255;
 
             Scalar wLower(whmin, wsmin, wvmin);
             Scalar wUpper(whmax, wsmax, wvmax);
-            //Yellow values
+            //Yellow mins and maxes for Hue-Saturation-Value model
             int yhmin = 19, yhmax = 49, ysmin = 34, ysmax = 126, yvmin = 175, yvmax = 255;
 
             Scalar yLower(yhmin, ysmin, yvmin);
@@ -106,57 +117,69 @@ public:
             // createTrackbar("Val min","Trackbars",&vmin,255);
             // createTrackbar("Val max","Trackbars",&vmax,255);
 
+            //Create ImageProcessor (From Header file)
             ImageProcessor i;
+
+            //Gets the blurred images for they white and yellow lines
             Mat yBlur = i.getBlur(yLower, yUpper, croppedImage);
             Mat wBlur = i.getBlur(wLower, wUpper, croppedImage);
 
+            //Gets the erroded images from canny edge detection (Images shown in black and white when file is run)
             Mat yErodeMat = i.getErode(yBlur);
             Mat wErodeMat = i.getErode(wBlur);
 
-            vector<vector<double>> yellowLaneLines;
-            vector<vector<double>> whiteLaneLines;
+            /** -------------------------------------------------**\
+            * -------------------Line Detection------------------- *
+            \**--------------------------------------------------**/
 
+            //Create vectors to hold slopes and intercepts of lines found with Hough Transform
+            vector<vector<double> > yellowLaneLines;
+            vector<vector<double> > whiteLaneLines;
+
+            //Processes images using Hough Transform and adds all slopes and intercepts along the bottom of image to the vector
             yellowLaneLines = i.processImage(yErodeMat, yBlur);
             whiteLaneLines = i.processImage(wErodeMat, wBlur);
 
+            //PRINTS ALL INTERCEPTS AND SLOPES OF YELLOW LINES
             for (int i = 0; i < static_cast<int>(yellowLaneLines.size()); i++)
             {
                 ROS_INFO("Yellow X coordinate: %s", std::to_string(yellowLaneLines.at(i).at(0)).c_str());
                 ROS_INFO("Yellow Slope: %s", std::to_string(yellowLaneLines.at(i).at(1)).c_str());
             }
 
+            //PRINTS ALL INTERCEPTS AND SLOPES OF WHITE LINES
             for (int i = 0; i < static_cast<int>(whiteLaneLines.size()); i++)
             {
                 ROS_INFO("White X coordinate: %s", std::to_string(whiteLaneLines.at(i).at(0)).c_str());
                 ROS_INFO("White Slope: %s", std::to_string(whiteLaneLines.at(i).at(1)).c_str());
             }
 
+            //Finds total number of lanes found
             int numLanes = static_cast<int>(whiteLaneLines.size()) + static_cast<int>(yellowLaneLines.size());
             ROS_INFO("NUMBER OF LANES FOUND: %s", std::to_string(numLanes).c_str());
 
+
+            /** -------------------------------------------------**\
+            * ----------------Steering Calculation----------------- *
+            \**--------------------------------------------------**/
+
+            
+            //IF BOTH COLORS OF LANES ARE FOUND, REDETERMINE LANE (USING FUNCTION DEFINED BELOW)
             if (static_cast<int>(whiteLaneLines.size()) >= 1 && static_cast<int>(yellowLaneLines.size()) >= 1)
             {
                 laneNumber = laneFinder(whiteLaneLines, yellowLaneLines, laneNumber);
             }
-
             ROS_INFO("Lane Number: %s", std::to_string(laneNumber).c_str());
 
+            //CREATE STEERING CONTROLER FOR CONSTANT SPEED
             ControlConst c;
 
+            //Function returns steering angle and speed (always 0.75 in this case) based on line lines and lane number
             vector<double> speedSteer = c.steerSpeed(yellowLaneLines, whiteLaneLines, laneNumber);
             carSpeed = speedSteer.at(0);
             steeringAngle = speedSteer.at(1);
 
-            // if(abs(steeringAngle) < 0.1){
-            //     carSpeed = 2.0;
-            // } else if(abs(steeringAngle) < 0.25){
-            //     carSpeed = 1.5;
-            // } else if(abs(steeringAngle) < 0.35){
-            //     carSpeed = 1.0;
-            // } else{
-            //     carSpeed = 0.75;
-            // }
-
+            //Sets output images
             woutput = wErodeMat;
             youtput = yErodeMat;
         }
@@ -165,8 +188,15 @@ public:
             ROS_ERROR("cv_bridge exception: %s", e.what());
             return;
         }
+
         ROS_INFO("Angle: %s", std::to_string(steeringAngle).c_str());
         ROS_INFO("Car Speed: %s", std::to_string(carSpeed).c_str());
+
+
+        /** -------------------------------------------------**\
+        * ---------------------Publishing--------------------- *
+        \**--------------------------------------------------**/
+
         //UPLOAD DRIVE DATA
         ackermann_msgs::AckermannDriveStamped drive_st_msg;
         ackermann_msgs::AckermannDrive drive_msg;
@@ -174,14 +204,16 @@ public:
         drive_msg.speed = carSpeed;
         drive_st_msg.drive = drive_msg;
         drive_pub.publish(drive_st_msg);
-        //
+
         // Update GUI Window
         cv::imshow(OPENCV_WINDOW_WHITE, woutput);
         cv::imshow(OPENCV_WINDOW_YELLOW, youtput);
         imshow(OTHER_WINDOW_WHITE, displayForWhiteLines);
         imshow(OTHER_WINDOW_YELLOW, displayForYellowLines);
+
         //wait time is in miliseconds
         cv::waitKey(3);
+
         //test stuff
         cv_bridge::CvImage yout_msg;
         yout_msg.header = msg->header;                                // Same timestamp and tf frame as input image
@@ -200,33 +232,32 @@ public:
         image_pub_.publish(wout_msg.toImageMsg());
     }
 
+    //FINDS WHICH LANE THE CAR IS CURRENTLY IN
     int
-    laneFinder(vector<vector<double>> wLines, vector<vector<double>> yLines, int lane)
+    laneFinder(vector<vector<double> > wLines, vector<vector<double> > yLines, int lane)
     {
+        //USES THE X COORDINATE OF EACH OF THE LINES TO DETERMINE LANE
         int laneFind = lane;
-        if (laneFind > -2)
+        if (yLines.size() > 0)
         {
-            if (yLines.size() > 0)
+            if (yLines.at(0).at(0) > 480)
             {
-                if (yLines.at(0).at(0) > 480)
-                {
-                    laneFind = 0;
-                }
-                else
-                {
-                    laneFind = 1;
-                }
+                laneFind = 0;
             }
-            else if (wLines.size() > 0)
+            else
             {
-                if (wLines.at(0).at(0) < 480)
-                {
-                    laneFind = 0;
-                }
-                else
-                {
-                    laneFind = 1;
-                }
+                laneFind = 1;
+            }
+        }
+        else if (wLines.size() > 0)
+        {
+            if (wLines.at(0).at(0) < 480)
+            {
+                laneFind = 0;
+            }
+            else
+            {
+                laneFind = 1;
             }
         }
         return laneFind;
